49829302124a2f7f24a4cff1bf72cea235d11f0e
izaack107 <izaack107@gmail.com>
2025-02-11 09:25:12 -0600

    ADD SharedLib

--- vars/dockerUtils.groovy
+++ vars/dockerUtils.groovy
@@ -1,9 +1,33 @@
-def buildAndPushImage(String imageName) {
-    echo "Building Docker image: ${imageName}"
-    sh "docker build -t ${imageName} ."
-    
-    echo "Pushing Docker image: ${imageName}"
-    sh "docker push ${imageName}"
+// vars/dockerUtils.groovy
+def buildAndPushImage(String fullImageTag, String dockerUser, String dockerPass, String localAlias) {
+    echo "Logging in to Docker Hub as ${dockerUser}..."
+    sh '''
+      echo "${dockerPass}" | docker login -u "${dockerUser}" --password-stdin
+    '''
+
+    echo "Tagging local image ${localAlias}:v1.0 as ${fullImageTag} and also tagging as latest"
+    // Tag and push the image with version tag.
+    sh "docker tag ${localAlias}:v1.0 ${fullImageTag}"
+    sh "docker push ${fullImageTag}"
+
+    // Tag and push the image with the "latest" tag.
+    def latestTag = fullImageTag.replaceAll(/:v1\.0$/, ':latest')
+    sh "docker tag ${localAlias}:v1.0 ${latestTag}"
+    sh "docker push ${latestTag}"
+
+    sh "docker logout"
 }
 
+def stopAndRemoveContainers(String deployPort) {
+    echo "Stopping and removing any running containers on port ${deployPort}..."
+    sh """
+      containers=\$(docker ps -q --filter "publish=${deployPort}")
+      if [ ! -z "\$containers" ]; then
+        docker stop \$containers
+        docker rm \$containers
+      else
+        echo "No running containers found for port ${deployPort}"
+      fi
+    """
+}
 return this
---
+++ vars/sharedLib.groovy
@@ -0,0 +1,5 @@
+// vars/sharedLib.groovy
+def announce() {
+    echo "Shared library integrated successfully!"
+}
+return this
